b6d1f4ed821ed689a6897cf70f968be319ad86f4
LucasParra95 <lucasandresparra@gmail.com>
2024-08-03 16:33:31 -0300

    Pruebas para fuentes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import Router from "next/router";
 import { wrapper } from "../store";
+//import { Poppins } from 'next/font/google'
 
 // types
 import type { AppProps } from "next/app";
@@ -17,6 +18,14 @@ import { SessionProvider } from "next-auth/react"
 import * as gtag from "./../utils/gtag";
 
 const isProduction = process.env.NODE_ENV === "production";
+
+ 
+// const poppins = Poppins({ 
+//     weight: "200",
+//     display: 'swap',
+//     variable: '--font-oxygen',
+//     subsets: ['latin'],
+// })
 
 // only events on production
 if (isProduction) {
